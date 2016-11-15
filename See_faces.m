ae60f5e0f30b0f73eab26fe38c90b0e3c3e42fd3
JHertz5 <jukka.hertzog@live.co.uk>
2016-11-15 14:06:08 +0000

    Efficiency improvements and comments for See_faces.m

--- See_faces.m
+++ See_faces.m
@@ -1,18 +1,23 @@
 clc
-clear all
+clear variables %clear all deletes cached code, slows things down
 close all
 
 load 'face.mat'
 
+%extract individual faces from X
+indvFaces = zeros(56, 46, 520, 'double'); %declare indvFaces for efficiency
 for j = 1:520
     for i = 1:46
-        indvFaces(1:56,i,j) = flipud(fliplr(X((i-1)*56+1:i*56,j)));
+        indvFaces(1:56,i,j) = rot90(X((i-1)*56+1:i*56, j), 2);
     end
 end
 
+%calculate average face for each face set
+aveFaces = zeros(56, 46, 52, 'double'); %declare aveFaces for efficiency
 for i = 1:52
     aveFaces(:,:,i) = mean(indvFaces(:,:,(i-1)*10+1:10*i),3);
 end
+
 figure(1)
 for j = 1:9
     subplot(3,3,j)
