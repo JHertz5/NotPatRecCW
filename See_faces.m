9cbf14aa4be2e01c09193ef47870e8182b26b560
JHertz5 <jukka.hertzog@live.co.uk>
2016-11-16 17:53:29 +0000

    Readbility improvements

--- See_faces.m
+++ See_faces.m
@@ -1,22 +1,29 @@
+%% initial setup
 clc
 close all
 
 load 'face.mat'
 
-%extract individual faces from X
-indvFaces = zeros(56, 46, 520, 'double'); %pre-allocate indvFaces for efficiency
-for j = 1:520
-    for i = 1:46
-        indvFaces(1:56,i,j) = rot90(X((i-1)*56+1:i*56, j), 2);
+%% extract individual faces from X
+
+faceW = 46; faceH = 56; nFaceSets = 52; lenFaceSet = 10; nFaces = nFaceSets * lenFaceSet;
+
+indvFaces = zeros(faceH, faceW, nFaces, 'double'); %pre-allocate indvFaces for efficiency
+for j = 1:nFaces
+    for i = 1:faceW %extract image one line at a time
+        indvFaces(1:faceH,i,j) = rot90(X((i-1)*faceH+1:i*faceH, j), 2);
     end
 end
 
-%calculate average face for each face set
-aveFaces = zeros(56, 46, 52, 'double'); %pre-allocate aveFaces for efficiency
-for i = 1:52
-    aveFaces(:,:,i) = mean(indvFaces(:,:,(i-1)*10+1:10*i),3);
+%% calculate average face for each face set
+aveFaces = zeros(faceH, faceW, nFaceSets, 'double'); %pre-allocate aveFaces for efficiency
+for i = 1:nFaceSets
+    faceStart = (i-1)*lenFaceSet+1;
+    faceEnd = i*lenFaceSet;
+    aveFaces(:,:,i) = mean(indvFaces(:,:,faceStart:faceEnd),3);
 end
 
+%% show figures
 if (exist('showFigures', 'var') && showFigures == true)
     figure(1)
     for j = 1:9
--- extract_ave_faces.m
+++ extract_ave_faces.m
@@ -7,36 +7,38 @@ close all
 
 %% calculate X = abs(face-mean) for each face and for each X find avergae pixel value
 
-%pre-allocate for efficiency
-absZeroMeanFaces = zeros(56, 46, 520, 'double');
-avePixVal = zeros(1, 520, 'double');
+faceW = 46; faceH = 56; nFaceSets = 52; lenFaceSet = 10; nFaces = nFaceSets * lenFaceSet;
 
-for i = 1:520
+%pre-allocate for efficiency
+absZeroMeanFaces = zeros(faceH, faceW, nFaces, 'double');
+avePixVal = zeros(1, nFaces, 'double');
+
+for i = 1:nFaces
     absZeroMeanFaces(:,:,i) = abs(indvFaces(:,:,i) - aveFaces(:,:,ceil(i/10)));
     avePixVal(i) = mean(mean(absZeroMeanFaces(:,:,i)));
 end
 
 %% for each class find 2 indices corresponding to most average face
 %  most average face will have average pixel value closest to zero
-actual_idx = zeros(1,104);
+actual_index = zeros(1,104);
 for j = 1:2
-    for i = 1:52
-        [~, idx] = min(avePixVal((i-1)*10+1:10*i));
+    for i = 1:nFaceSets
+        [~, index] = min(avePixVal((i-1)*10+1:10*i));
         if j == 1
-            actual_idx(2*i-1) = idx + (i-1)*10;
-            avePixVal(actual_idx(2*i-1)) = 1000;
+            actual_index(2*i-1) = index + (i-1)*10;
+            avePixVal(actual_index(2*i-1)) = 1000;
         else
-            actual_idx(2*i) = idx + (i-1)*10;
-            avePixVal(actual_idx(2*i)) = 1000;
+            actual_index(2*i) = index + (i-1)*10;
+            avePixVal(actual_index(2*i)) = 1000;
         end
     end
 end
 
 %% sort indices in the ascending order
-actual_idx = sort(actual_idx); % these indices correspond to the 2 most average faces in each class
+actual_index = sort(actual_index); % these indices correspond to the 2 most average faces in each class
 
 %% separate face data into training and testing data
-testing = X(:,actual_idx);
+testing = X(:,actual_index);
 training = X;
-training(:,actual_idx)=[];
+training(:,actual_index)=[];
 save('Separated_Data','testing','training')
