394a2a2f0ef6081bec2b1526c2d4e07e956251db
JHertz5 <jukka.hertzog@live.co.uk>
2016-12-11 21:40:07 +0000

    Separated Q3 raw and PCA

--- scripts/Q3_MultiSVM_PCA.m
+++ scripts/Q3_MultiSVM_PCA.m
@@ -24,22 +24,22 @@ load Separated_Data.mat
 load face.mat
 
 %% Set test parameter
-testImage = 31; % <- the only user variable here
+testImageIndex = 31; % <- the only user variable here
 
-testingImage1 = testing(:,(testImage-1)*2+1)';
-testingImage2 = testing(:,(testImage-1)*2+2)';
+testingImage1 = testing(:,(testImageIndex-1)*2+1)';
+testingImage2 = testing(:,(testImageIndex-1)*2+2)';
 
 %% Compute One vs One SVM with my function
 [class1] = OVOSVM(testingImage1,training);
 [class2] = OVOSVM(testingImage2,training);
 
-if class1 == testImage
+if class1 == testImageIndex
     fprintf('First image recognised correctly!\n');
 else
     fprintf('First image not recognised...\n');
 end
 
-if class2 == testImage
+if class2 == testImageIndex
     fprintf('Second image recognised correctly!\n');
 else
     fprintf('Second image not recognised...\n');
