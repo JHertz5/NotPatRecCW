dabd3fb771bf0eff776f2ce914983c3b6fc8ed40
JHertz5 <jukka.hertzog@live.co.uk>
2016-12-12 21:21:21 +0000

    Changed multiSVM to test all testing images. Still need to do PCA version

--- scripts/Q3_MultiSVM_raw.m
+++ scripts/Q3_MultiSVM_raw.m
@@ -24,25 +24,35 @@ load Separated_Data.mat
 load face.mat
 
 %% Set test parameter
-testImageIndex = 31; % <- the only user variable here
+%testImageIndex = 31; % <- the only user variable here
 
-testingImage1 = testing(:, (testImageIndex-1)*2+1)';
-testingImage2 = testing(:,  (testImageIndex-1)*2+2)';
+numClasses = size(testing, 2)/2;
+accuracyVector = zeros(1, size(testing, 2), 'logical');
 
-%% Compute One vs One SVM with my function
-[class1] = OVOSVM(testingImage1,training);
-[class2] = OVOSVM(testingImage2,training);
-%[class1] = OVASVM(testingImage1, testImageIndex, training);
-%[class2] = OVASVM(testingImage2, testImageIndex, training);
-
-if class1 == testImageIndex
-    fprintf('First image recognised correctly!\n');
-else
-    fprintf('First image not recognised...\n');
-end
-
-if class2 == testImageIndex
-    fprintf('Second image recognised correctly!\n');
-else
-    fprintf('Second image not recognised...\n');
+for testClassIndex = 1:numClasses
+    
+    testingImage1 = testing(:, (testClassIndex-1)*2+1)';
+    testingImage2 = testing(:,  (testClassIndex-1)*2+2)';
+    
+    %% Compute One vs One SVM with my function
+    %[classAssignment1] = OVOSVM(testingImage1,training);
+    %[classAssignment2] = OVOSVM(testingImage2,training);
+    [classAssignment1] = OVASVM(testingImage1, testClassIndex, training);
+    [classAssignment2] = OVASVM(testingImage2, testClassIndex, training);
+    
+    if classAssignment1 == testClassIndex
+        fprintf('Class %i - First image recognised correctly!\n', testClassIndex);
+        accuracyVector(testClassIndex*2) = true;
+    else
+        fprintf('Class %i - First image not recognised\n', testClassIndex);
+        accuracyVector(testClassIndex*2) = false;
+    end
+    
+    if classAssignment2 == testClassIndex
+        fprintf('Class %i - Second image recognised correctly!\n', testClassIndex);
+        accuracyVector(testClassIndex*2 + 1) = true;
+    else
+        fprintf('Class %i - image not recognised\n', testClassIndex);
+        accuracyVector(testClassIndex*2 + 1) = false;
+    end
 end
