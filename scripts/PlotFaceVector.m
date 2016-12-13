8ad4ad529d0939a57055672148b81c0e898cfe77
JHertz5 <jukka.hertzog@live.co.uk>
2016-12-13 16:41:50 +0000

    Added PlotFaceVector function

---
+++ scripts/PlotFaceVector.m
@@ -0,0 +1,24 @@
+function [faceMatrix] = PlotFaceVector( faceWidth, faceHeight, faceVector, plotNow )
+%Function to plot a face vector
+
+faceMatrix = zeros(faceHeight, faceWidth, 'double');
+
+for i = 1:faceWidth %extract image one line at a time
+    lineStart = (i-1)*faceHeight + 1;
+    lineEnd = i*faceHeight;
+    
+    faceMatrix(1:faceHeight,i) = rot90(faceVector(lineStart:lineEnd), 2);
+    
+end
+
+if plotNow
+    figure
+    h = pcolor(faceMatrix);
+    set(h,'edgecolor','none');
+    colormap gray
+    shading interp
+    %ylabel('First Success Case')
+    set(gca,'XtickLabel',[],'YtickLabel',[]);
+end
+
+end
